ceee06f0c87b6826ef1d9b964ddb485cd815589f
Joseph Tatum <josephtatum@gmail.com>
2019-11-07 10:40:02 -0800

    created server.js file with Seth Ripman

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const pg = require('pg');
 
 // Database Client
 // (create and connect using DATABASE_URL)
-const Client = pg.client;
+const Client = pg.Client;
 const client = new Client(process.env.DATABASE_URL);
 client.connect();
 
@@ -25,13 +25,33 @@ app.use(cors());
 app.use(express.static('./public'));
 app.use(morgan('dev'));
 // API Routes
+app.get('/api/dannys', async (req, res) => {
+    try {
+        const result = await client.query(`
+            SELECT
+                id,
+                name,
+                age,
+                profession,
+                has_dignity as hasDignity,
+                power_level as "powerLevel"
+            FROM DANNYS;
+        `);
 
+        res.json(result.rows);
+    }
+    catch (err) {
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+
+});
 // http method and path...
 
 
 // Start the server
 // (use PORT from .env!)
-
 
 app.listen(PORT, () => {
     console.log('server running on PORT', PORT);
