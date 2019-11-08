75bc84c6124c2d894c04ba6efd04945896d4a2cf
sethripman <sethripman@gmail.com>
2019-11-08 09:50:26 -0800

    wrote drop tables

--- data/sample.js
+++ data/sample.js
@@ -1,27 +1,49 @@
+// load connection string from .env
 require('dotenv').config();
+// "require" pg (after `npm i pg`)
 const pg = require('pg');
+// Use the pg Client
 const Client = pg.Client;
+// **note:** you will need to create the database!
 
+// async/await needs to run in a function
 run();
 
 async function run() {
+    // make a new pg client to the supplied url
     const client = new Client(process.env.DATABASE_URL);
 
     try {
+        // initiate connecting to db
         await client.connect();
-    
+
+        // run a query to create tables
         await client.query(`
-            DROP TABLE IF EXISTS cats;
-            DROP TABLE IF EXISTS types;
+            CREATE TABLE types (
+                id SERIAL PRIMARY KEY NOT NULL,
+                name VARCHAR(256) NOT NULL
+            );
+
+            CREATE TABLE cats (
+                id SERIAL PRIMARY KEY NOT NULL,
+                name VARCHAR(256) NOT NULL,
+                type_id INTEGER NOT NULL REFERENCES types(id),
+                url VARCHAR(256) NOT NULL,
+                year INTEGER NOT NULL,
+                lives INTEGER NOT NULL,
+                is_sidekick BOOLEAN NOT NULL
+            );
         `);
 
-        console.log('drop tables complete');
+        console.log('create tables complete');
     }
     catch (err) {
+        // problem? let's see the error...
         console.log(err);
     }
     finally {
+        // success or failure, need to close the db connection
         client.end();
     }
-    
+
 }
